6cdea61ec3e7e288cca327b8ebeec581b1289c27
Michael Smith <smithatlanta@gmail.com>
2019-09-12 16:50:39 -0400

    trying to see whats going on.

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,10 @@ fi
 mkdir -p ~/.aws
 touch ~/.aws/credentials
 
+echo ${SOURCE_DIR}
+
+ls -al ${SOURCE_DIR}
+
 echo "[default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
