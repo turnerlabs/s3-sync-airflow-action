30e9e05860f4c9d72b0eb80ae0854864f31fd3c3
Michael Smith <mike.smith@turner.com>
2019-09-13 15:38:48 -0400

    more verboseness

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,19 +35,19 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
 if [ -d "$SOURCE_DIR/variables" ]; then
-    aws s3 sync $SOURCE_DIR/variables s3://${AWS_S3_BUCKET}/variables --exact-timestamps --debug --delete --region ${AWS_DEFAULT_REGION} $*
+    aws s3 sync ${SOURCE_DIR}/variables s3://${AWS_S3_BUCKET}/variables --exact-timestamps --debug --delete --region ${AWS_DEFAULT_REGION} $*
 fi
 
 if [ -d "$SOURCE_DIR/requirements" ]; then
-    aws s3 sync $SOURCE_DIR/requirements s3://${AWS_S3_BUCKET}/requirements --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
+    aws s3 sync ${SOURCE_DIR}/requirements s3://${AWS_S3_BUCKET}/requirements --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 fi
 
 if [ -d "$SOURCE_DIR/dags" ]; then
-    aws s3 sync $SOURCE_DIR/dags s3://${AWS_S3_BUCKET}/dags --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
+    aws s3 sync ${SOURCE_DIR}/dags s3://${AWS_S3_BUCKET}/dags --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 fi
 
 if [ -d "$SOURCE_DIR/plugins" ]; then
-    aws s3 sync $SOURCE_DIR/plugins s3://${AWS_S3_BUCKET}/plugins --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
+    aws s3 sync ${SOURCE_DIR}/plugins s3://${AWS_S3_BUCKET}/plugins --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 fi
 
 rm -rf ~/.aws
