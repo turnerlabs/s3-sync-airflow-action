d8009e283d443959adf981d7e853e342e1665e62
Michael Smith <mike.smith@turner.com>
2019-09-16 10:28:30 -0400

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -36,6 +36,8 @@ aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}" > ~/.aws/credentials
 
  aws configure list
+ 
+ aws s3 ls s3://${AWS_S3_BUCKET}
 
 if [ -d "$SOURCE_DIR/variables" ]; then
     aws s3 sync ${SOURCE_DIR}/variables s3://${AWS_S3_BUCKET}/variables --exact-timestamps --debug --delete --region ${AWS_DEFAULT_REGION} $*
