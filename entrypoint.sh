ca171fbd9bd33ce63582e8f75df8ecbb06756a9a
Michael Smith <smithatlanta@gmail.com>
2019-10-25 15:11:49 -0400

    when you paste code look closely at what you paste :(

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,7 +38,7 @@ if [ -d "$SOURCE_DIR/variables" ]; then
     echo "Copying to variables folder"
     aws s3 sync ${SOURCE_DIR}/variables s3://${AWS_S3_BUCKET}/variables --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
-  if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/variables/`" != "" ]; then
+  if [ "`aws s3 ls s3://$AWS_S3_BUCKET/variables/`" != "" ]; then
       echo "Remove variables folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/variables/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/variables
@@ -50,7 +50,7 @@ if [ -d "$SOURCE_DIR/requirements" ]; then
     echo "Copying to requirements folder"
     aws s3 sync ${SOURCE_DIR}/requirements s3://${AWS_S3_BUCKET}/requirements --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
-  if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/requirements/`" != "" ]; then
+  if [ "`aws s3 ls s3://$AWS_S3_BUCKET/requirements/`" != "" ]; then
       echo "Remove requirements folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/requirements/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/requirements
@@ -62,7 +62,7 @@ if [ -d "$SOURCE_DIR/dags" ]; then
     echo "Copying to dags folder"
     aws s3 sync ${SOURCE_DIR}/dags s3://${AWS_S3_BUCKET}/dags --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
-  if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/dags/`" != "" ]; then
+  if [ "`aws s3 ls s3://$AWS_S3_BUCKET/dags/`" != "" ]; then
       echo "Remove dags folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/dags/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/dags
@@ -74,7 +74,7 @@ if [ -d "$SOURCE_DIR/plugins" ]; then
     echo "Copying to plugins folder"
     aws s3 sync ${SOURCE_DIR}/plugins s3://${AWS_S3_BUCKET}/plugins --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
-  if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/plugins/`" != "" ]; then
+  if [ "`aws s3 ls s3://$AWS_S3_BUCKET/plugins/`" != "" ]; then
       echo "Remove plugins folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/plugins/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/plugins
@@ -86,7 +86,7 @@ if [ -d "$SOURCE_DIR/sql" ]; then
     echo "Copying to sql folder"
     aws s3 sync ${SOURCE_DIR}/sql s3://${AWS_S3_BUCKET}/sql --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
-  if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/sql/`" != "" ]; then
+  if [ "`aws s3 ls s3://$AWS_S3_BUCKET/sql/`" != "" ]; then
       echo "Remove sql folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/sql/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/sql
@@ -98,7 +98,7 @@ if [ -d "$SOURCE_DIR/turner_lib" ]; then
     echo "Copying to turner_lib folder"
     aws s3 sync ${SOURCE_DIR}/turner_lib s3://${AWS_S3_BUCKET}/turner_lib --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
-  if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/turner_lib/`" != "" ]; then
+  if [ "`aws s3 ls s3://$AWS_S3_BUCKET/turner_lib/`" != "" ]; then
       echo "Remove turner_lib folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/turner_lib/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/turner_lib
