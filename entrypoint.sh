49d32366a0c6c5e773106f7ac292b5b594b3082b
Michael Smith <mike.smith@turner.com>
2019-10-25 14:49:09 -0400

    added logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,18 +38,21 @@ if [ -d "$SOURCE_DIR/variables" ]; then
     aws s3 sync ${SOURCE_DIR}/variables s3://${AWS_S3_BUCKET}/variables --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
   if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/variables/`" != "" ]; then
+      echo "Remove variables folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/variables/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/variables
+      echo "Remove variables folder complete"
   fi
 fi
 
 if [ -d "$SOURCE_DIR/requirements" ]; then
     aws s3 sync ${SOURCE_DIR}/requirements s3://${AWS_S3_BUCKET}/requirements --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
-
   if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/requirements/`" != "" ]; then
+      echo "Remove requirements folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/requirements/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/requirements
+      echo "Remove requirements folder completed"      
   fi
 fi
 
@@ -57,8 +60,10 @@ if [ -d "$SOURCE_DIR/dags" ]; then
     aws s3 sync ${SOURCE_DIR}/dags s3://${AWS_S3_BUCKET}/dags --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
   if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/dags/`" != "" ]; then
+      echo "Remove dags folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/dags/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/dags
+      echo "Remove dags folder completed"      
   fi
 fi
 
@@ -66,8 +71,10 @@ if [ -d "$SOURCE_DIR/plugins" ]; then
     aws s3 sync ${SOURCE_DIR}/plugins s3://${AWS_S3_BUCKET}/plugins --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
   if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/plugins/`" != "" ]; then
+      echo "Remove plugins folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/plugins/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/plugins
+      echo "Remove plugins folder completed"      
   fi
 fi
 
@@ -75,8 +82,10 @@ if [ -d "$SOURCE_DIR/sql" ]; then
     aws s3 sync ${SOURCE_DIR}/sql s3://${AWS_S3_BUCKET}/sql --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
   if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/sql/`" != "" ]; then
+      echo "Remove sql folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/sql/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/sql
+      echo "Remove sql folder completed"      
   fi
 fi
 
@@ -84,8 +93,10 @@ if [ -d "$SOURCE_DIR/turner_lib" ]; then
     aws s3 sync ${SOURCE_DIR}/turner_lib s3://${AWS_S3_BUCKET}/turner_lib --exact-timestamps --delete --region ${AWS_DEFAULT_REGION} $*
 else
   if [ "`aws s3 ls s3://$S3_AIRFLOW_BUCKET/turner_lib/`" != "" ]; then
+      echo "Remove turner_lib folder started"
       aws s3 rm s3://${AWS_S3_BUCKET}/turner_lib/ --recursive
       aws s3 rm s3://${AWS_S3_BUCKET}/turner_lib
+      echo "Remove turner_lib folder completed"      
   fi
 fi
 
